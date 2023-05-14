2c80f0928754de0cb7f4e3b5b99caa43e922aa2e
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2023-05-14 09:31:59 +0200

    initialee

---
+++ WEB/v7-backend/src/main/java/rs/ac/uns/ftn/svtvezbe07/model/entity/User.java
@@ -0,0 +1,39 @@
+package rs.ac.uns.ftn.svtvezbe07.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private LocalDateTime LastLogin;
+    @Column(nullable = false)
+    private String firstname;
+    @Column(nullable = false)
+    private String lastname;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Roles role;
+}
